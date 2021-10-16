cb760507ca4f9a61f82a10fd04dbf8311cac8b99
SuanCaiYv <jokerblack2508826394@gmail.com>
2021-10-16 10:13:43 +0800

    初始提交

--- main/main.go
+++ main/main.go
@@ -1,12 +1,22 @@
 package main
 
-import (
-	"GoBaseLearn/src"
-)
-
 /*
 之前写了一份base版放在另一台电脑里了，现在重新写一份简略版的基础语法
 */
+
+type MyInterface interface {
+	Print()
+	Show()
+}
+
+type MyStruct struct {
+}
+
+func (m MyStruct) Print() {}
+
+func (m MyStruct) Show() {}
+
 func main() {
-	src.ShowConcurrent()
+	var myStruct MyInterface = &MyStruct{}
+	myStruct.Show()
 }
---
+++ src/web.go
@@ -0,0 +1,9 @@
+package src
+
+import "net"
+
+func simple() {
+	listener, _ := net.Listen("ipv4", ":8190")
+	connection, _ := listener.Accept()
+	connection.Close()
+}
