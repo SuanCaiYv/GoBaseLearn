d6711fd345146df4d857e7ca8e2bccb37848bfdf
SuanCaiYv <jokerblack2508826394@gmail.com>
2022-01-06 20:39:38 +0800

    进阶内容

--- main/main.go
+++ main/main.go
@@ -1,25 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 /*
 之前写了一份base版放在另一台电脑里了，现在重新写一份简略版的基础语法
 */
 
-type MyInterface interface {
-	Print()
-	Show()
-}
-
-type MyStruct struct {
-}
-
-func (m MyStruct) Print() {}
-
-func (m MyStruct) Show() {}
-
 func main() {
-	var a interface{}
-	a = MyStruct{}
-	fmt.Println(a.(MyStruct))
+	type T struct {
+		a int8
+		b int32
+		c int16
+		d int64
+		e int16
+	}
+	t := T{}
+	fmt.Println(unsafe.Alignof(t.a))
+	fmt.Println(unsafe.Alignof(t.b))
+	fmt.Println(unsafe.Alignof(t.c))
+	fmt.Println(unsafe.Alignof(t.d))
+	fmt.Println(unsafe.Sizeof(t))
 }
