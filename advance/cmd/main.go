c6dbc86eaf58d3b8155dfd3963c053ab091397b9
SuanCaiYv <jokerblack2508826394@gmail.com>
2022-03-18 00:34:08 +0800

    schedule

--- advance/cmd/main.go
+++ advance/cmd/main.go
@@ -1,41 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
-
-type T struct {
-	a int64
-	b int64
-	c int64
-	d int64
-}
-
 func main() {
-	PtrSize := 4 << (^uintptr(0) >> 63)
-	uintptrMask := 1<<(8*PtrSize) - 1
-	fmt.Println(uintptrMask & -1314)
-	f()
-}
-
-func test1() (t T) {
-	t = T{1, 2, 3, 4}
-	return t
-}
-
-func test2() T {
-	v := T{1, 2, 3, 4}
-	return v
-}
-
-func f() {
-	defer func() {
-		fmt.Println("bbb")
-		fmt.Println(recover())
+	go func() {
+		sum := 0
+		for i := 0; i < 100; i += 1 {
+			sum += i
+		}
 	}()
-	defer func() {
-		fmt.Println("ccc")
-	}()
-	panic("aaa")
-	fmt.Println("ddd")
+	sum0 := 0
+	for i0 := 0; i0 < 100; i0 += 2 {
+		sum0 += i0
+	}
 }
