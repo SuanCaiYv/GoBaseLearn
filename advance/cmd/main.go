5f325511ab940f75b6a11c4652ccaab5ce53fdc2
SuanCaiYv <jokerblack2508826394@gmail.com>
2022-03-19 19:48:24 +0800

    netpoll

--- advance/cmd/main.go
+++ advance/cmd/main.go
@@ -1,14 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"net"
+)
+
 func main() {
-	go func() {
-		sum := 0
-		for i := 0; i < 100; i += 1 {
-			sum += i
-		}
-	}()
-	sum0 := 0
-	for i0 := 0; i0 < 100; i0 += 2 {
-		sum0 += i0
+	listen, _ := net.Listen("tcp", ":8080")
+	accept, err := listen.Accept()
+	if err != nil {
+		return
 	}
+	fmt.Println(accept)
 }
