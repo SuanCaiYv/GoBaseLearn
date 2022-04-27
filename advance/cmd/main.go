d7b2a2d445fea0d7ad15253e780c59215631c84d
SuanCaiYv <jokerblack2508826394@gmail.com>
2022-04-27 23:57:39 +0800

    closure

--- advance/cmd/main.go
+++ advance/cmd/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 type MyInterface interface {
 	Foo()
 }
@@ -11,7 +16,44 @@ func (m *MyType) Foo() {
 }
 
 func main() {
-	var i MyInterface
-	i = &MyType{}
-	i.Foo()
+	closure1()
+	time.Sleep(1 * time.Second)
+	closure2()
+	time.Sleep(1 * time.Second)
+	closure3()
+	time.Sleep(1 * time.Second)
+}
+
+func closure1() {
+	work := make(chan struct{})
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			<-work
+			fmt.Println("1: ", i)
+		}(i)
+	}
+	close(work)
+}
+
+func closure2() {
+	work := make(chan struct{})
+	for i := 0; i < 10; i++ {
+		j := i
+		go func() {
+			<-work
+			fmt.Println("2: ", j)
+		}()
+	}
+	close(work)
+}
+
+func closure3() {
+	work := make(chan struct{})
+	for i := 0; i < 10; i++ {
+		go func() {
+			<-work
+			fmt.Println("3: ", i)
+		}()
+	}
+	close(work)
 }
